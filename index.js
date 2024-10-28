083fe4c42fde1430bbe879651f481e2854c2db1a
FabricioDag <git@stackblitz.com>
2024-10-28 08:49:54 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3002; // Porta da API
+
+// Endpoint para servir os arquivos PDF armazenados na pasta 'uploads'
+app.use('/files', express.static(path.join(__dirname, 'uploads')));
+
+app.listen(PORT, () => {
+  console.log(`API rodando na porta ${PORT}`);
+});
